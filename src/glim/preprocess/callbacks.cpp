b3b16712b579c0da44e284dae53730dc2a4a43a8
k.koide <k.koide@aist.go.jp>
2025-12-25 18:44:34 +0900

    add callbacks for preprocessing steps

--- src/glim/preprocess/callbacks.cpp
+++ src/glim/preprocess/callbacks.cpp
@@ -2,4 +2,8 @@
 
 namespace glim {
 CallbackSlot<void(const RawPoints::ConstPtr& points)> PreprocessCallbacks::on_raw_points_received;
+
+CallbackSlot<void(gtsam_points::PointCloudCPU::Ptr& points)> PreprocessCallbacks::on_preprocessing_begin;
+CallbackSlot<void(gtsam_points::PointCloudCPU::Ptr& points)> PreprocessCallbacks::on_downsampling_finished;
+CallbackSlot<void(gtsam_points::PointCloudCPU::Ptr& points)> PreprocessCallbacks::on_filtering_finished;
 }  // namespace glim
